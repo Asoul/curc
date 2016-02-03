4a2bc69b6f68babaf2d40417875397a8803dbe84
Asoul <azx754@gmail.com>
2016-02-03 20:49:09 +0800

    sort data by time, remove redundant column

--- script/trans_old.py
+++ script/trans_old.py
@@ -1,11 +1,15 @@
 import csv
 import json
+import time
+from datetime import datetime
 import unicodecsv
 
 json_file = open("../Record.json", "r")
 data = json.load(json_file)
 
 field_names = [
+    "createdAt",
+    "bank",
     "AUD",
     "AUDout",
     "CAD",
@@ -33,20 +37,24 @@ field_names = [
     "USD",
     "USDout",
     "ZAR",
-    "ZARout",
-    "bank",
-    "createdAt",
-    "isSuccess",
-    "message",
-    "objectId",
-    "updateTime",
-    "updatedAt",
+    "ZARout"
 ]
 
+# Load file
 csv_file = open('../data/record.csv', 'w')
-writer = unicodecsv.DictWriter(csv_file, fieldnames=field_names)
+writer = unicodecsv.DictWriter(csv_file,
+    fieldnames=field_names, extrasaction='ignore')
 
-writer.writeheader()
-
+rows = []
 for row in data["results"]:
+    rows.append(row)
+
+# Sort by time
+rows.sort(key=lambda row:
+    time.mktime(datetime.strptime(row["createdAt"],
+        "%Y-%m-%dT%H:%M:%S.%fZ").timetuple()))
+
+# Write file
+writer.writeheader()
+for row in rows:
     writer.writerow(row)
